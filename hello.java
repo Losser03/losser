c606cd0e84cebae7e4b3cf33991b6588abe9b1bc
Losser <mogullamanohar03@gmail.com>
2023-01-07 22:37:42 +0530

    Thshshshhshs

---
+++ hello.java
@@ -0,0 +1,26 @@
+public class hello{
+    public static void main(String[] args)
+    {
+        System.out.println("Helloooo Losser");
+        //Data Types
+        String name="Mahesh";
+        System.out.println(name);
+        int a=999;
+        System.out.println("This is integer a is"+a);
+        double madhu=666111.333333333;
+        float ee=9154143;
+        System.out.println(madhu);
+        System.out.println(ee);00
+
+//data Types   3SINGLE LINE COMMENT
+      /*MULTI LINE COMMENT
+      1.STRING
+      2.INTEGER(int)
+      3.LONG(long)
+      4.FLOAT(float)
+      5..DOUBLE(double)
+      6.SHORT(short)
+
+       */
+    }
+}
